da31cbd785a9205997e1950748876d11f4933823
karimabd84 <abdelkarim.esti@gmail.com>
2017-08-15 15:50:18 +0200

    AngularJs App for consuming WebApi added

---
+++ AngularJsAuthentication/Providers/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,41 @@
+﻿using AngularJsAuthentication.Repositories;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AngularJsAuthentication.Providers
+{
+    public class SimpleAuthorizationServerProvider: OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            //enabling CORS (Cross Origin Resource Sharing) in Web API as well for the OAuth bearer token provider.
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            using (AuthRepository _repo = new AuthRepository())
+            {
+                IdentityUser user = await _repo.FindUser(context.UserName, context.Password);
+                if(user==null)
+                {
+                    context.SetError("invalid_grant", "The user name or password is incorrect.");
+                    return;
+                }
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+            context.Validated(identity);
+        }
+    }
+}
